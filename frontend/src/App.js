8bf0d71cd58a8fbb89e1800974c87045916a7198
Jin garam <2001garam@naver.com>
2021-10-05 14:28:25 +0900

    Home page product listing with Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <h1>Welcome to Kajin</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Home />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Kajin</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fa fa-shopping-cart' /> Cart
+              </Nav.Link>
+              <Nav.Link href='/signin'>
+                <i className='fa fa-user' /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
