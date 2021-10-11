9919be554c65f913dd2dc8563e87cf1017e2180a
Jin garam <2001garam@naver.com>
2021-10-11 15:09:27 +0900

    Custom error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -24,7 +24,10 @@ router.get(
     const product = await Product.findById(req.params.id);
 
     if (product) res.json(product);
-    else res.status(404).json({ message: 'Product not found' });
+    else {
+      res.status(404); // 설정 안하면 500
+      throw new Error('Product not found');
+    }
   })
 );
 
