7f3112cbb0b428c0ba2a9d4fcae0e4c64f1374cb
Jin garam <2001garam@naver.com>
2021-10-19 21:53:25 +0900

    Register page & reducer, action

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  Col,
+  Form,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Row,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { register } from '../actions/userActions';
+import FormContainer from '../components/Form';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
+// 로그인 화면에 필요한 상태: 이메일과 패스워드
+const RegisterPage = ({ location, history }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, userInfo, error } = userRegister;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    } // 이미 로그인 되어있다면 로그인 화면 보이지 않기
+  }, [userInfo, history, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // DISPATCH REGISTER: 버튼을 눌렀을 시 회원 정보를 보낸다.
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match');
+    } else {
+      setMessage('');
+      dispatch(register(name, email, password));
+    }
+  };
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {message && <Message variant='danger'>{message}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <FormGroup controlId='name'>
+          <FormLabel>Name</FormLabel>
+          <FormControl
+            type='name'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></FormControl>
+        </FormGroup>
+
+        <FormGroup controlId='email'>
+          <FormLabel>Email Address</FormLabel>
+          <FormControl
+            type='email'
+            placeholder='Enter email address'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></FormControl>
+        </FormGroup>
+
+        <FormGroup controlId='password'>
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></FormControl>
+        </FormGroup>
+
+        <FormGroup controlId='confirmPassword'>
+          <FormLabel>Confirm Password</FormLabel>
+          <FormControl
+            type='password'
+            placeholder='Enter the password again'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></FormControl>
+        </FormGroup>
+
+        <Button type='submit' variant='primary'>
+          Register
+        </Button>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          Have an Account already?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/login'}>
+            Login
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default RegisterPage;
