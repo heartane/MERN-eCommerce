e074e8366610c09298a2e9492d67d1c8d8ca2893
Jin garam <2001garam@naver.com>
2021-11-07 22:12:13 +0900

    Add paid order  in controller & reducer and action

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { addOrderItems, GetOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  GetOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/auth.js'; // for private routes
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, GetOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 export default router;
