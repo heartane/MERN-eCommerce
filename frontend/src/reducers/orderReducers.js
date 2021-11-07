be968b77a67004bedfc3feaed1b21ea021534a3f
Jin garam <2001garam@naver.com>
2021-11-08 01:18:23 +0900

    Reset user details & order list when user logout

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -12,6 +12,7 @@ import {
   ORDER_MY_LIST_REQUEST,
   ORDER_MY_LIST_SUCCESS,
   ORDER_MY_LIST_FAIL,
+  ORDER_MY_LIST_RESET,
 } from '../constants/orderConstants.js';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -78,6 +79,9 @@ export const orderMyListReducer = (state = { orders: [] }, action) => {
     }
     case ORDER_MY_LIST_FAIL: {
       return { loading: false, error: action.payload };
+    }
+    case ORDER_MY_LIST_RESET: {
+      return { orders: [] };
     }
     default:
       return state;
